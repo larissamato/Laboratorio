04769407bf745cbe134eda56bdb9fd2f931962f7
larissamato <matoslarissa31@gmail.com>
2022-03-25 08:35:33 -0300

    "aula2"

---
+++ Aula_02/01_Exe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float number1, number2, result=0.0;
+    printf("\t\t\t\t\t\t ARITHMETIC MEAN CALCULATION\n");
+    printf("Enter the first number: \n");
+    scanf();
+    printf("Enter the second number: \n");
+    result= (number1+number2)/2.0;
+    printf("The result is: %f", result);
+    printf("\n");
+    return 0;
+}
