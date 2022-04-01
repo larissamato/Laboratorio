922bd3a25367c672bdf3829508430524f3a267ae
larissamato <matoslarissa31@gmail.com>
2022-04-01 11:41:19 -0300

    if e else

---
+++ Aula_03/04_ifElse.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int nota =6;
+    
+    if(nota>=6)
+    {
+        printf("Parabéns! Você passou! \n");
+    }
+    else
+    {
+        printf("Inútil! \n");
+    }
+    return 0;
+
+}
