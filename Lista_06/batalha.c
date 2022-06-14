799dbccc2dd327db602b2f9f946f9dae21155b89
larissamato <matoslarissa31@gmail.com>
2022-06-14 13:00:04 -0300

    print batalha

--- Lista_06/batalha.c
+++ Lista_06/batalha.c
@@ -5,27 +5,34 @@
 
 int x=0,y=0,v=0,count=0,pos=0,verx=0,vero=0,verv=0;
 int linha=0,coluna=0;
-char matriz[10][22];
+char matriz[11][22];
 
 void gerar(){
-    //matriz[0][2]='0';
-    matriz[0][4]='1';
-    matriz[0][6]='2';
-    matriz[0][8]='3';
-    matriz[0][10]='4';
-    matriz[0][12]='5';
-    matriz[0][14]='6';
-    matriz[0][16]='7';
-    matriz[0][18]='8';
-    matriz[0][20]='9';
+    int count=0;
     for(int i=0; i<1;i++){
         for(int j=0;j<22;j++){
-             if(j%2!=0){
+            if(j==0){
+            matriz[i][j]=' ';    
+            }
+            else if(j%2!=0){
             matriz[i][j]=' ';
-        }
+            }
+            else if(j%2==0){
+            matriz[i][j]=count+'0';
+            count++;
+            }
+
         }
     }
-    for(int i=1;i<10;i++){
+    
+    for(int i=0; i<1;i++){
+        for(int j=0;j<22;j++){
+            if(j%2!=0){
+            matriz[i][j]=' ';
+            }
+        }
+    }
+    for(int i=1;i<11;i++){
         for(int j=0;j<22;j++)
         if(j%2!=0){
             matriz[i][j]='|';
@@ -34,25 +41,62 @@ void gerar(){
             matriz[i][j]=' ';
         }
     }
+    count=0;
+    for(int i=1; i<11;i++){
+        for(int j=0;j==0;j++){
+            matriz[i][j]=count+'0';
+            count++;
+        }
+    }
   
 }
 
 void print_matriz(){
-    system("clear"); //limpa tela windows
-    //system("clear"); linux e mac
-    for (x=0;x<10;x++){
-        for (y=0;y<21;y++){
+    system("clear");
+    for (x=0;x<11;x++){
+        for (y=0;y<22;y++){
             printf("%c", matriz[x][y]);
         }
         printf("\n");
     }
 }
 
+void sorteio(){
+int naviox[9];
+int navioy[9];
+int count = 1,x=0,y=0;
+
+for (int i=0; i<9; i++){
+    naviox[i]= rand() % 9;
+}
+
+for (int i=0; i<9; i++){
+    navioy[i]= rand() % 9;
+}
+
+for (int i=0; i<9; i++){
+    printf("\t%d ", navio[i]);
+}
+printf("\nDigite o valor de x e y do tabuleiro: ");
+scanf("%d %d", &x, &y);
+
+for (int i=0; i<10; i++){
+    for (int j=0; j<10; j++){
+        if (x == naviox[i] && y == navioy[j]){
+        matriz[i][j]='o';
+        gerar();
+        }
+    }
+}
+}
+
 int main ()
 
 {
+
    gerar();
    print_matriz();
+   sorteio();
 
     /*char strings [5][100];
     int count;
@@ -73,37 +117,3 @@ int main ()
 
     return(0);
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-int linha=0,coluna=0;
-char matriz[10][10];
-int main (){
-int navio[9];
-int count = 1;
-
-for (int i=0; i<9; i++){
-    navio[i]= rand() % 100;
-}
-for (int i=0; i<9; i++){
-    printf("\t%d ", navio[i]);
-}
-for (int i=0; i<10; i++){
-    for (int j=0; j<10; j++){
-        count ++;
-        if (count == navio[1]){
-            
-        }
-    }
-}
-return 0;
-}*/
